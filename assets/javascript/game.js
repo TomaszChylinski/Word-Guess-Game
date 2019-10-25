188c19f36c522dae4b2f9e81ab45c4a6323ab14a
chylinski.t <chylinski.t@outlook.com>
2019-10-24 23:11:35 -0400

    added alphabet and cpu words arrarys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,65 @@
-    //keyUp function 
-        //document.getElementById("userInput");
-        var userText = document.getElementById("userInput");
-        document.onkeyup  = function(event){
-            userText.textContent = event.key;
-            console.log(userText);
-        };
+//onKeyUp function to start the game
+
+var userText = $("userInput");
+onkeyup = function(keyClicked) {
+  userText = keyClicked.key;
+  console.log(userText);
+  return userText;
+};
+
+//arrary of letters user has to chose from
+var letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "_"
+];
+
+
+// an arrary of words for the cpu to chose from
+var cpuGuestWords = [
+  "ghost",
+  "scary",
+  "dog",
+  "funny",
+  "master",
+  "light",
+  "soccer",
+  "wonderful",
+  "tinkle",
+  "haberdasher",
+  "basketball",
+  "finance",
+  "purple",
+  "equivalent",
+  "interstellar"
+];
+
+var cpuWordChoice =
+  cpuGuestWords[Math.floor(Math.random() * cpuGuestWords.length)];
+console.log(cpuWordChoice);
+
+//print out my letters
