87487806e4b89cf254388e92a31fb46bb0833700
chylinski.t <chylinski.t@outlook.com>
2019-10-26 00:06:59 -0400

    fixed my cpuWordChoice undefined error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,11 @@
+//declaring variables
 var remainingGuesses = 10;
-var wins = 0;
-var loses = 0;
-
-var userText = document.getElementById("lettersUsed");
-
-// Next, we give JavaScript a function to execute when onkeyup event fires.
-document.onkeyup = function(event) {
-  userText.textContent = event.key;
-  console.log(userText.textContent);
-};
-
-//aplphabet arrary
-var alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
+var wins;
+var losses;
+var rand;
+var cpuWordChoice;
+var answerArrary = [];
+var wrongLetters;
 
 // an arrary of words for the cpu to chose from
 var cpuGuestWords = [
@@ -54,18 +21,30 @@ var cpuGuestWords = [
 ];
 
 function startGame() {
+  //print out remainig guesses
+
+  document.getElementById("remainingGuesses").textContent = remainingGuesses;
+
   //pick random word
-  var cpuWordChoice =
+  cpuWordChoice =
     cpuGuestWords[Math.floor(Math.random() * cpuGuestWords.length)];
+  console.log(cpuWordChoice);
 
-
-  //create underscores for placeholders 
-  var answerArrary = [];
+  //create underscores for placeholders
   for (var i = 0; i < cpuWordChoice.length; i++) {
     answerArrary.push(" __ ");
   }
 
   document.getElementById("trysLeft").textContent = answerArrary.join(" ");
 }
+
+//store key event upon onkeyup
+document.onkeyup = function(event) {
+  userInput = event.key;
+
+  if (cpuWordChoice.indexOf(userInput) > -1) {
+    console.log('what is tthis ' + cpuWordChoice.indexOf(userInput));
+  }
+};
 
 startGame();
