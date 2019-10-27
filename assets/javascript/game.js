cd10091aaa6050292356c1f63b55445e8128b35b
chylinski.t <chylinski.t@outlook.com>
2019-10-26 22:56:09 -0400

    added lose and win counter, win isnt working properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,33 @@
 //declaring variables
 var remainingGuesses = 10;
-var wins = 0;
-var losses = 0;
 var rand;
 var cpuWordChoice;
 var answerArrary = [];
 var wrongLetter = [];
+var winCounter = 0;
+var losses = 0;
+var wins = 0;
 
 // an arrary of words for the cpu to chose from
 var cpuGuestWords = [
   "soccer",
   "wonderful",
   "tinkle",
-  "haberdasher",
+  "light",
   "basketball",
   "finance",
   "purple",
   "equivalent",
-  "interstellar"
+  "innter",
+  "space",
+  "moon",
+  "mars",
+  "color",
+  "game"
 ];
 
 function startGame() {
-
-//printing my variables onto the screen
+  //printing my variables onto the screen
   document.getElementById("remainingGuesses").textContent = remainingGuesses;
   document.getElementById("lost").textContent = losses;
   document.getElementById("win").textContent = wins;
@@ -35,48 +40,47 @@ function startGame() {
   //create underscores for placeholders
   for (var i = 0; i < cpuWordChoice.length; i++) {
     answerArrary.push(" __ ");
+    document.getElementById("currentWord").textContent = answerArrary.join(" ");
   }
 
-  document.getElementById("currentWord").textContent = answerArrary.join(" ");
-}
-
-//decide if user has won or lost
-function winLose() {
-  if (winCounter === cpuWordChoice.length) {
-    alert("Winner");
-  }
-}
-
-function loseGame() {
-  if (remainingGuesses < 1) {
-    document.getElementById("lost").textContent = losses++;
-    alert("Sorry You Lost");
-  }
-}
-
-//user guesses
-document.onkeyup = function(event) {
-  userInput = event.key;
-  //checking if letter exisits in cpuWordChoice word
-  if (cpuWordChoice.indexOf(userInput) > -1) {
-    for (var i = 0; i < cpuWordChoice.length; i++) {
-      //if true than replace underscore with userInput
-      if (cpuWordChoice[i] === userInput) {
-        answerArrary[i] = userInput;
-        document.getElementById("currentWord").textContent = answerArrary;
-        console.log("testing if underscore gets replaced " + answerArrary);
-        winLose();
-      }
+  //alert if user won or lost
+  function winLose() {
+    if (winCounter === cpuWordChoice.length) {
+      document.getElementById("win").textContent = wins++;
+      alert("Winner");
+    } else if (remainingGuesses <= 0) {
+      document.getElementById("lost").textContent = losses++;
+      alert("Sorry You Lost");
+      startGame();
     }
-  } else {
-    wrongLetter.push(userInput);
-
-    document.getElementById(
-      "remainingGuesses"
-    ).textContent = remainingGuesses--;
-    console.log(wrongLetter);
-    document.getElementById("lettersUsed").textContent = wrongLetter;
   }
-};
+
+  //user guesses
+  document.onkeyup = function(event) {
+    userInput = event.key;
+    //checking if letter exisits in cpuWordChoice word
+    if (cpuWordChoice.indexOf(userInput) > -1) {
+      for (var i = 0; i < cpuWordChoice.length; i++) {
+        //if true than replace underscore with userInput
+        if (cpuWordChoice[i] === userInput) {
+          answerArrary[i] = userInput;
+          winCounter++;
+          winLose();
+          document.getElementById("currentWord").textContent = answerArrary;
+          // console.log("testing if underscore gets replaced " + answerArrary);
+        }
+      }
+    } else {
+      wrongLetter.push(userInput);
+      remainingGuesses--;
+      document.getElementById(
+        "remainingGuesses"
+      ).textContent = remainingGuesses;
+      winLose();
+      console.log(wrongLetter);
+      document.getElementById("lettersUsed").textContent = wrongLetter;
+    }
+  };
+}
 
 startGame();
